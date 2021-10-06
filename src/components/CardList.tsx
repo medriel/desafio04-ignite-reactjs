112528693e9e5aeb0e150734b91a5fe9b5d893d6
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-06 20:02:02 -0300

    Desenvolviendo o CardList

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,5 @@
 import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { createReadStream } from 'fs';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -16,17 +17,28 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { onOpen, isOpen, onClose } = useDisclosure()
 
-  // TODO SELECTED IMAGE URL STATE
+  const [currentImageUrl, setCurrentImageUrl] = useState('')
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    onOpen()
+    setCurrentImageUrl(url)
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => {
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        })}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={currentImageUrl}
+        onClose={onClose}
+      />
     </>
   );
 }
