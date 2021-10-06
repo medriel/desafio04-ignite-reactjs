565618ed74532397af9d498b944de8166ddb893d
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-06 19:32:17 -0300

    Formatando mensagens de error para aparecer em tela caso necessario

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -89,20 +89,20 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-        // TODO SEND IMAGE ERRORS
-        // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          {...register('image', formValidations.image)}
+          error={errors.image}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-        // TODO SEND TITLE ERRORS
-        // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          {...register('title', formValidations.title)}
+          error={errors.title}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-        // TODO SEND DESCRIPTION ERRORS
-        // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          {...register('description', formValidations.description)}
+          error={errors.description}
         />
       </Stack>
 
