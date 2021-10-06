b5f45721bb886f479fbb62af055961f0bc42b94d
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-06 18:59:59 -0300

    desenvolvendo o useInfiniteQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,30 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string
+  description: string
+  url: string
+  ts: number
+  id: string
+}
+
+interface GetImagesResponse {
+  after: string
+  data: Image[]
+}
+
 export default function Home(): JSX.Element {
+
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam
+      }
+    })
+    return data
+  }
+
   const {
     data,
     isLoading,
@@ -17,11 +40,9 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+    'images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
